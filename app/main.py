c97288216c8315f5014396246b0dbbdc3560e0f9
Alexander Mot <alexander@amot.dev>
2026-03-26 23:58:14 -0700

    Move first admin creation into function

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from fastapi.staticfiles import StaticFiles
 
 from httpx import AsyncClient, HTTPStatusError
 from pydantic_core import ErrorDetails
-from sqlalchemy import func, select
 from starlette.middleware.sessions import SessionMiddleware
 import sys
 from time import time
@@ -19,10 +18,10 @@ from app.events import EventSet
 from app.models import User
 from app.redis_client import redis_client
 from app.routers import admin, auth, debug, ratings, twists, users
-from app.schemas.users import UserCreate
+from app.services.admin import create_first_admin
 from app.services.auth import login_and_set_response_cookie
 from app.settings import Settings, settings
-from app.users import UserManager, current_user_optional, get_user_db
+from app.users import current_user_optional
 from app.utility import format_loc_for_user, raise_http, sort_schema_names
 
 
@@ -33,24 +32,7 @@ async def lifespan(app: FastAPI):
     """
     async for session in get_db():
         # Create initial admin user
-        result = await session.execute(
-            select(func.count()).select_from(User)
-        )
-        user_count = result.scalar_one()
-        if user_count == 0:
-            user_data = UserCreate(
-                email=settings.MOTOTWIST_ADMIN_EMAIL,
-                password=settings.MOTOTWIST_ADMIN_PASSWORD,
-                is_active=True,
-                is_superuser=True,
-                is_verified=True,  # Force verification for initial admin to prevent oopsies
-            )
-            user_db = await anext(get_user_db(session))
-            user_manager = UserManager(user_db)
-            await user_manager.create(user_data)
-            logger.info(f"Admin user '{settings.MOTOTWIST_ADMIN_EMAIL}' created")
-        else:
-            logger.info("Admin user creation skipped")
+        await create_first_admin(session)
 
     yield
 
--- app/services/admin.py
+++ app/services/admin.py
@@ -5,7 +5,37 @@ from sqlalchemy.orm import Mapped
 from typing import cast
 from uuid import UUID
 
+from app.settings import settings
+from app.config import logger
 from app.models import User
+from app.schemas.users import UserCreate
+from app.users import UserManager, get_user_db
+
+
+async def create_first_admin(session: AsyncSession) -> bool:
+    """
+    TODO
+    """
+    result = await session.execute(
+        select(func.count()).select_from(User)
+    )
+    user_count = result.scalar_one()
+    if user_count == 0:
+        user_data = UserCreate(
+            email=settings.MOTOTWIST_ADMIN_EMAIL,
+            password=settings.MOTOTWIST_ADMIN_PASSWORD,
+            is_active=True,
+            is_superuser=True,
+            is_verified=True,  # Force verification for initial admin to prevent oopsies
+        )
+        user_db = await anext(get_user_db(session))
+        user_manager = UserManager(user_db)
+        await user_manager.create(user_data)
+        logger.info(f"Admin user '{settings.MOTOTWIST_ADMIN_EMAIL}' created")
+        return True
+    else:
+        logger.info("Admin user creation skipped")
+        return False
 
 
 async def is_last_active_admin(session: AsyncSession, user: User) -> bool:
